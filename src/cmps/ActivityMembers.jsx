657ae51e70d4ee3594cefc743bcee5870476554b
EdenAlmighty <edengilady1@gmail.com>
2024-04-03 21:17:02 +0300

    Sync Team

---
+++ src/cmps/ActivityMembers.jsx
@@ -0,0 +1,8 @@
+export function ActivityMembers(){
+    
+    return (
+        <button>
+            Activity
+        </button>
+    )
+}
--- src/cmps/Board/BoardHeader.jsx
+++ src/cmps/Board/BoardHeader.jsx
@@ -1,10 +1,13 @@
+import {MyWork} from '../../services/svg.service.jsx'
 
 export function BoardHeader() {
 
     return (
         <section className="board-header-wrapper">
-            <div className="board-header-top">
+            <div className="board-header-top flex">
                 <h2>Start from scratch</h2>
+                {/* <ActivityMembers/> */}
+        <MyWork/>
             </div>
         </section>
     )
