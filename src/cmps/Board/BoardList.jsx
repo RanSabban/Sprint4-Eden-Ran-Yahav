5fc78f1ff7ff00edc859cf73f6aeafabb320b67d
YahavGanon <yahavganon86@gmail.com>
2024-04-06 19:33:38 +0300

    Adding option to elemenate actions while click outside of them

--- src/cmps/Board/BoardList.jsx
+++ src/cmps/Board/BoardList.jsx
@@ -1,172 +1,148 @@
 import React, { useEffect, useState, useRef } from "react"
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { DeleteSvg, DuplicateSvg, FavoritesSvg, NewTab, RenameSvg, SidePrevSvg, ThreePoints } from "../../services/svg.service"
-import { addBoard, loadBoards } from "../../store/board.actions"
-import { boardService } from "../../services/board.service.local"
 import { BoardListPreview } from "./BoardListPreview"
 
-export function BoardList({ boards, onAddBoard, onRemoveBoard, onUpdateBoard }) {
+function Board({ board, onUpdateBoard, onRemoveBoard }) {
     const [isEdit, setIsEdit] = useState(false)
     const [isShown, setIsShown] = useState(false)
-    const [editedTitle, setEditedTitle] = useState("")
+    const [editedTitle, setEditedTitle] = useState(board.title)
     const inputRef = useRef(null)
-    const [currentBoard, setCurrentBoard] = useState(null)
-    console.log('the current board please: ', currentBoard)
-    console.log('IS IT?', isShown)
 
-    // useEffect(() => {
-    //     const handleClickOutside = (event) => {
-    //         if (inputRef.current && !inputRef.current.contains(event.target)) {
-    //             setIsEdit(false)
-    //             setIsShown(false)
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (inputRef.current && !inputRef.current.contains(event.target)) {
+                setIsEdit(false)
+                setIsShown(false)
+    
+                if (editedTitle.trim() !== "") {
+                    onUpdateBoard({ ...board, title: editedTitle })
+                }
+            }
+        }
+    
+        document.addEventListener("mousedown", handleClickOutside)
+    
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [editedTitle, board, onUpdateBoard])
+    
+    const handleOutsideClick = (event) => {
+        if (isShown && !event.target.closest(".actions-border-side")) {
+            setIsShown(false)
+        }
+    }
+    
+    useEffect(() => {
+        document.addEventListener("mousedown", handleOutsideClick)
+    
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick)
+        }
+    }, [isShown])
 
-    //             if (editedTitle.trim() !== "" && currentBoard) {
-    //                 const updatedBoard = { ...currentBoard, title: editedTitle }
-    //                 onUpdateBoard(updatedBoard)
-    //             }
-    //             setEditedTitle("")
-    //             setCurrentBoard(null)
-    //         }
-    //     }
-
-    //     document.addEventListener("mousedown", handleClickOutside)
-
-    //     return () => {
-    //         document.removeEventListener("mousedown", handleClickOutside)
-    //     }
-    // }, [editedTitle, currentBoard, onUpdateBoard])
-
-    function changeBoardName(board) {
+    function changeBoardName() {
         if (!isEdit) {
             setEditedTitle(board.title)
-            setCurrentBoard(board)
         }
         setIsEdit(!isEdit)
     }
 
-    function onSubmitTitle(ev, board) {
+    function onSubmitTitle(ev) {
         ev.preventDefault()
-
-        board.title = editedTitle
-        onUpdateBoard(board)
-        setIsEdit(!isEdit)
-        setIsShown(!isShown)
+        onUpdateBoard({ ...board, title: editedTitle })
+        setIsEdit(false)
+        setIsShown(false)
     }
 
-    function removeBoard(boardId) {
-        console.log('board from remove ', boardId)
-        onRemoveBoard(boardId)
-        setIsShown(!isShown)
+    function removeBoard() {
+        onRemoveBoard(board._id)
+        setIsShown(false)
     }
 
-    function duplicateBoard(ev, board) {
+    function duplicateBoard(ev) {
         ev.preventDefault()
-
         delete board._id
         onUpdateBoard(board)
-        setIsShown(!isShown)
+        setIsShown(false)
     }
 
-
-    // async function duplicateBoard(ev, board) {
-    //     ev.preventDefault()
-
-    //     try {
-    //         const savedBoard = await addBoard(board)
-    //         setIsShown(!isShown)
-    //         showSuccessMsg(`Board added (id: ${savedBoard._id})`)
-    //     } catch (err) {
-    //         showErrorMsg('Cannot add board')
-    //     }
-    // }
-
-
-    function addToFavorites(ev, board) {
+    function addToFavorites(ev) {
         ev.preventDefault()
-
-        board.isStarred = !board.isStarred
-        onUpdateBoard(board)
-        setIsShown(!isShown)
+        onUpdateBoard({ ...board, isStarred: !board.isStarred })
+        setIsShown(false)
     }
 
-    function openInNewTab(board) {
-        setIsShown(!isShown)
+    function openInNewTab() {
+        setIsShown(false)
         window.open(`http://localhost:5173/board/${board._id}`, '_blank', 'noopener')
     }
 
     return (
+        <NavLink
+            className="board-side-preview"
+            style={{ textDecoration: "none", display: "flex", gap: "1em", color: "#323338" }}
+            to={`/board/${board._id}`}
+            key={board._id}
+        >
+            <div className="board-list-prev">
+                <SidePrevSvg />
+                {!isEdit && <BoardListPreview board={board} />}
+                {!isEdit && (
+                    <button onClick={() => setIsShown(!isShown)} className="preview-three-points">
+                        <ThreePoints />
+                    </button>
+                )}
+            </div>
+            {!isEdit && (
+                <div>
+                    <div style={{ display: isShown ? "block" : "none" }} className={`actions-border-side`}>
+                        <div onClick={openInNewTab} style={{ lineHeight: "20px" }} className="action-side">
+                            <NewTab />
+                            <p>Open Board in New Tab</p>
+                        </div>
+                        <hr style={{ width: '100%', color: "#323338" }} />
+                        <div onClick={removeBoard} style={{ lineHeight: "20px" }} className="action-side">
+                            <DeleteSvg />
+                            <p>Delete</p>
+                        </div>
+                        <div onClick={changeBoardName} style={{ lineHeight: "20px" }} className="action-side">
+                            <RenameSvg />
+                            <p>Rename Board</p>
+                        </div>
+                        <div onClick={duplicateBoard} style={{ lineHeight: "20px" }} className="action-side">
+                            <DuplicateSvg />
+                            <p>Duplicate Board</p>
+                        </div>
+                        <div onClick={addToFavorites} style={{ lineHeight: "20px" }} className="action-side">
+                            <FavoritesSvg />
+                            {board.isStarred ? <p>Remove from favorites</p> : <p>Add to favorites</p>}
+                        </div>
+                    </div>
+                </div>
+            )}
+            {isEdit && (
+                <form onSubmit={onSubmitTitle}>
+                    <input style={{ width: '175px' }} type="text" ref={inputRef} value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
+                </form>
+            )}
+        </NavLink>
+    )
+}
 
+export function BoardList({ boards, onAddBoard, onRemoveBoard, onUpdateBoard }) {
+    return (
         <section className="board-list">
             {boards.map((board) => (
-
-                <NavLink onClick={() => setCurrentBoard(board)}
-                    className="board-side-preview"
-                    style={{ textDecoration: "none", display: "flex", gap: "1em", color: "#323338" }}
-                    to={`/board/${board._id}`}
-                    key={board._id}>
-                    <div className="board-list-prev">
-                        <SidePrevSvg />
-                        {!isEdit && <BoardListPreview board={board} />}
-                        {!isEdit && <button onClick={() => setIsShown(!isShown)} className="preview-three-points">
-                            <ThreePoints />
-                        </button>}
-                    </div>
-                    {!isEdit && (
-                        <div>
-                            <div style={{ display: isShown ? "block" : "none" }} className={`actions-border-side`}>
-
-                                <div
-                                    onChange={() => setIsShown(!isShown)}
-                                    onClick={() => openInNewTab(currentBoard)}
-                                    style={{ lineheight: "20px" }}
-                                    className="action-side"
-                                >
-                                    <NewTab />
-                                    <p>Open Board in New Tab</p>
-                                </div>
-                                <hr style={{ width: '100%', color: "#323338" }} />
-                                <div
-                                    onClick={() => removeBoard(currentBoard._id)}
-                                    style={{ lineheight: "20px" }}
-                                    className="action-side"
-                                >
-                                    <DeleteSvg />
-                                    <p>Delete</p>
-                                </div>
-                                <div
-                                    onClick={() => changeBoardName(currentBoard)}
-                                    style={{ lineHeight: "20px" }}
-                                    className="action-side"
-                                >
-                                    <RenameSvg />
-                                    <p>Rename Board</p>
-                                </div>
-
-                                <div
-                                    onClick={(ev) => duplicateBoard(ev, currentBoard)}
-                                    style={{ lineheight: "20px" }}
-                                    className="action-side"
-                                >
-                                    <DuplicateSvg />
-                                    <p>Duplicate Board</p>
-                                </div>
-
-                                <div
-                                    onClick={(ev) => addToFavorites(ev, currentBoard)}
-                                    style={{ lineheight: "20px" }}
-                                    className="action-side"
-                                >
-                                    <FavoritesSvg />
-                                    {board.isStarred ? <p>Add to favorites</p> : <p>Remove from favorites</p>}
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                    {isEdit && <form onSubmit={(ev) => onSubmitTitle(ev, board)}><input style={{width: '175px'}} type="text" ref={inputRef} value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} /></form>}
-                </NavLink>
-            ))
-            }
-        </section >
+                <Board
+                    key={board._id}
+                    board={board}
+                    onUpdateBoard={onUpdateBoard}
+                    onRemoveBoard={onRemoveBoard}
+                />
+            ))}
+        </section>
     )
 }
 
