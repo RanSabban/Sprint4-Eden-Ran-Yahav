28f1edec168a4d4f4b7a36de4f51a95b715a2764
Ran Sabban <sabbanran@gmail.com>
2024-04-12 13:25:01 +0300

    Finish basic board home

--- src/cmps/Board/BoardHomeList.jsx
+++ src/cmps/Board/BoardHomeList.jsx
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux"
 import { useParams } from "react-router"
 import { loadBoards } from "../../store/actions/board.actions"
 import { useEffect } from "react"
+import { Board } from "monday-ui-react-core/icons"
+import { Link } from "react-router-dom"
 
 export function BoardHomeList() {
 
@@ -16,11 +18,24 @@ export function BoardHomeList() {
 
     console.log(boards);
 
-    
 
+    if (!boards) return
     return <section className="board-home-list">
 
-        
-        
+        {boards.map(board => (
+            <Link to={`/board/${board._id}`}
+                className="board-home-card-container"
+                key={board._id}
+            >
+                <img class="quick-search-item-image" src="https://cdn.monday.com/images/quick_search_recent_board.svg" />
+            <section className="board-home-card-title">
+                    <Board />
+                    <span>{board.title}</span>
+                </section>
+            </Link>
+        ))}
+
+
     </section>
 }
+
