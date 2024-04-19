63f594a06e9ccc7297f56c538a6f8487cf21177e
YahavGanon <yahavganon86@gmail.com>
2024-04-19 16:21:16 +0300

    Fix layout on login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -21,27 +21,13 @@ export function LoginPage() {
                         <div className="separator-line"></div>
                     </div>
                         <LoginSignup/>
-                    <section className='work-email'>
-                        <span>Work email</span>
-                        <input className='work-email-input rect' type="email" required placeholder='name@company.com' />
-                        <Link to='/board'>
-                        <button className='rect'>Continue</button>
-                        </Link>
-                    </section>
-                    <div className="terms">
-                        <p>By proceeding, you agree to the</p>
-                        <div className='terms-links-wrapper'>
-                        <a href="#">Terms of Service</a>
-                        <p> and </p>
-                        <a href="#">Privacy Policy</a>
-                        </div>
-                    </div>
+    
                 </section>
 
-                <footer>
+                {/* <footer>
                     <p>Already have an account?</p>
                     <a href="#">Log in</a>
-                </footer>
+                </footer> */}
             </div>
 
             <aside className='img-container' >
