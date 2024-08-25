485fd9ce3795aa98ae8b5bc7f0177408122c6961
EdenAlmighty <edengilady1@gmail.com>
2024-08-25 15:33:08 +0300

    Fix signup/login alignment and user image

--- src/cmps/LoginSignup.jsx
+++ src/cmps/LoginSignup.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
 import { login, signup } from '../store/actions/user.actions'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { Link, useNavigate } from 'react-router-dom'
-
 export function LoginSignup() {
     const [credentials, setCredentials] = useState({ username: '', password: '', fullname: '', avatar: null })
     const [isSignup, setIsSignup] = useState(false)
-    const [avatarPreview, setAvatarPreview] = useState(null)
+    const [avatarPreview, setAvatarPreview] = useState('https://res.cloudinary.com/dkvliixzt/image/upload/v1704358773/person-empty_zckbtr_wrffbw.svg')
     const navigate = useNavigate()
 
     function handleChange(ev) {
@@ -60,7 +58,7 @@ export function LoginSignup() {
 
     return (
         <div className="login-signup">
-            {isSignup &&
+            {isSignup ? (
                 <form onSubmit={handleSubmit}>
                     <label htmlFor='fullname'>Full Name</label>
                     <input
@@ -73,23 +71,6 @@ export function LoginSignup() {
                         onChange={handleChange}
                         required
                     />
-
-                    <section className="avatar-upload">
-                        <label htmlFor="avatar-upload" className="avatar-label">
-                            {avatarPreview ? (
-                                <img src={avatarPreview} alt="Avatar Preview" className="avatar-image" />
-                            ) : (
-                                <div className="avatar-placeholder">Upload Avatar</div>
-                            )}
-                        </label>
-                        <input
-                            type="file"
-                            id="avatar-upload"
-                            style={{ display: 'none' }}
-                            accept="image/*"
-                            onChange={handleFileChange}
-                        />
-                    </section>
 
                     <label htmlFor='username'>Username</label>
                     <input
@@ -115,11 +96,68 @@ export function LoginSignup() {
                         required
                     />
 
-                    <button type="submit">{isSignup ? 'Sign Up' : 'Log In'}</button>
-                </form>
-            }
+                    <section className="avatar-upload" style={{ cursor: 'pointer' }}>
+                        <label htmlFor="avatar-upload" className="avatar-label" style={{ cursor: 'pointer' }}>
+                            <img src={avatarPreview} alt="Avatar Preview" className="avatar-image" style={{ cursor: 'pointer' }} />
 
-            {/* The rest of the form for login/signup toggling */}
+                            <div className="avatar-placeholder" style={{ cursor: 'pointer' }}>Upload Avatar</div>
+
+                        </label>
+                        <input
+                            type="file"
+                            id="avatar-upload"
+                            style={{ display: 'none' }}
+                            accept="image/*"
+                            onChange={handleFileChange}
+                        />
+                    </section>
+
+
+                    <button type="submit">Sign Up</button>
+                </form>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='username'>Username</label>
+                    <input
+                        className='input-login'
+                        type="text"
+                        name="username"
+                        id='username'
+                        value={credentials.username}
+                        placeholder="Username"
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        className='input-login'
+                        type="password"
+                        name="password"
+                        id='password'
+                        value={credentials.password}
+                        placeholder="Password"
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <button type="submit">Log In</button>
+                </form>
+            )}
+
+            <div className="terms">
+                <p>By proceeding, you agree to the</p>
+                <div className='terms-links-wrapper'>
+                    <a href="#">Terms of Service</a>
+                    <p> and </p>
+                    <a href="#">Privacy Policy</a>
+                </div>
+            </div>
+            {isSignup ? (
+                <p>Already have an account? <span className='signup-option' style={{ color: '#1f76c2' }} onClick={() => setIsSignup(false)}>Login</span></p>
+            ) : (
+                <p>Don't have an account yet? <span className='signup-option' style={{ color: '#1f76c2' }} onClick={() => setIsSignup(true)}>Sign up</span></p>
+            )}
         </div>
     )
 }
